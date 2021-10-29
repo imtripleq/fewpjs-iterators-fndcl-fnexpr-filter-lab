08abd8d9061a4219f3dd07503e8ccf13d8ac7116
imtripleq <flamework89@hotmail.com>
2021-10-30 08:30:59 +1100

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solution here
+const findMatching = (driver, string) => {
+  const b = driver.filter((w) => {
+    if (w === string) return w === string;
+    else return w.toLowerCase() === string.toLowerCase();
+  });
+  return b;
+};
+
+const drivers = ["Bobby", "Sammy", "Sally", "Annette", "Sarah", "Bobby"];
+
+const fuzzyMatch = (a, b) => {
+  return a.filter(
+    (y) => y.charAt(0).toLowerCase().indexOf(b.charAt(0).toLowerCase()) !== -1
+  );
+  ////// Worked for the test, but not in real world. It detects only first character.
+  // return a.filter((y) => y.toLowerCase().indexOf(b.toLowerCase()) !== -1);
+};
+
+fuzzyMatch(drivers, "s");
+const matchName = (a, b) => {
+  return a.filter((y) => y.name.toLowerCase().indexOf(b.toLowerCase()) !== -1);
+};
